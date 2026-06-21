5e00560ac5d287212fb634e12aa49637a7bd835a
puddly <32534428+puddly@users.noreply.github.com>
2026-06-21 12:30:41 -0400

    Support any exported `arbitrary_int` unsigned type

--- tests/bitfield.rs
+++ tests/bitfield.rs
@@ -8,6 +8,15 @@ struct Register {
     count: u2,
 }
 
+#[derive(Debug, PartialEq)]
+#[abstract_bits]
+struct UnalignedPack {
+    field_a: u27,
+    field_b: u7,
+    field_c: u37,
+    field_d: u9,
+}
+
 // #[abstract_bits]
 // struct NormalStruct {
 //     list: [bool; 5],
@@ -26,4 +35,39 @@ fn main() {
 
     // assert_eq!(NormalStruct::MIN_BITS, NormalStruct::MAX_BITS);
     // assert_eq!(NormalStruct::MIN_BITS, 5);
+}
+
+#[test]
+fn arbitrary_int_packing() {
+    let s = UnalignedPack {
+        field_a: 0b101_11111101_11111111_01111111,
+        field_b: 0b10_10011,
+        field_c: 0b1111011_11011111_11111111_01111111_111011,
+        field_d: 0b10001000_1,
+    };
+
+    let bytes = s.to_abstract_bits().unwrap();
+    assert_eq!(
+        bytes,
+        vec![
+            // field_a
+            0b01111111,
+            0b11111111,
+            0b11111101,
+            // field_a + field_b
+            0b10011_101,
+            // field_b + field_c
+            0b111011_10,
+            // field_c
+            0b01111111,
+            0b11111111,
+            0b11011111,
+            // field_c + field_d
+            0b1_1111011,
+            0b10001000,
+        ]
+    );
+
+    let parsed = StrangeInts::from_abstract_bits(&bytes).unwrap();
+    assert_eq!(parsed, s);
 }
